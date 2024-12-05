39deb3fcd4eeddfa0063de003ae818341f47732a
Maxim <zeetreex@gmail.com>
2024-12-05 15:15:42 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,59 @@
 
 import './App.css'
+import { Wrapper } from './components/wrapper'
 
 function App() {
-
-
   return (
     <>
-      <div className='h-12 bg-black text-white flex items-center justify-center space-x-2 relative'>
-        <p>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! </p>
-        <a href="#" className="underline">ShopNow</a>
-        <p className='absolute right-32 mr-5'>English</p>
-      </div>
+      <div className='h-12 bg-black text-white'>
+        <Wrapper>
+          <div className='flex justify-between items-center h-full'>
+            <div className='flex items-center'>
+              <span>Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</span>
+              <a href="#" className="underline ml-2">ShopNow</a>
+            </div>
 
-      <div className='h-24 flex items-center justify-between mx-36'>
-        <div className='text-2xl font-bold text-black'>
-          <a href="#">Exclusive</a>
-        </div>
-
-        <div className='flex space-x-8'>
-          <a href="#" className='hover:underline'>Home</a>
-          <a href="#" className='hover:underline'>Contact</a>
-          <a href="#" className='hover:underline'>About</a>
-          <a href="#" className='hover:underline'>Sign Up</a>
-        </div>
-
-        <div className='flex space-x-2'>
-          <div>
-            <input type="search" placeholder="What are you looking for?" className='w-full border-none bg-slate-100 px-4 ' />
+            <span>English</span>
           </div>
-          <p>Wishlist</p>
-          <p>Shopping card</p>
-        </div>
-
+        </Wrapper>
       </div>
 
-      <hr />
-      
+      <div className='border-b border-b-black'>
+        <Wrapper>
+          <div className='h-24 flex items-center justify-between '>
+            <div className='text-2xl font-bold text-black'>
+              <a href="#">Exclusive</a>
+            </div>
+
+            <ul className='flex gap-x-8'>
+              <li>
+                <a href="#" className='hover:underline underline-offset-1'>Home</a>
+              </li>
+              <li>
+                <a href="#" className='hover:underline underline-offset-1'>Contact</a>
+              </li>
+              <li>
+                <a href="#" className='hover:underline underline-offset-1'>About</a>
+              </li>
+              <li>
+                <a href="#" className='hover:underline underline-offset-1'>Sign Up</a>
+              </li>
+            </ul>
+
+            <div className='flex gap-x-2'>
+              <div>
+                <input
+                  type="search"
+                  placeholder="What are you looking for?"
+                  className='w-full border-none bg-slate-100 px-4'
+                />
+              </div>
+              <p>Wishlist</p>
+              <p>Shopping card</p>
+            </div>
+          </div>
+        </Wrapper>
+      </div>
     </>
   )
 }
